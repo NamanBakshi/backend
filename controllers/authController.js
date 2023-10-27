4ab6595bc2bb38bdd28142bc0d2b155cf2ca3f78
Naman Bakshi <110176142+NamanBakshi@users.noreply.github.com>
2023-10-28 02:08:48 +0530

    hell

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,7 +74,7 @@ const loginController=async (req,res)=>{
                 
                 
                 return  res
-                .cookie("token",token,{ maxAge: 900000})
+                .cookie("token",token,{ maxAge: 900000,domain:["localhost:5173,onrender.com"]})
                 .status(200).json({
                     success:true ,
                     message :"Login Successful" ,
